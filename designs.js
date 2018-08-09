94e615710f3f0af61c265c21e8c5cb84f6ce93cb
Jamie Kawahara <kawaharajamie@gmail.com>
2018-08-08 23:28:12 -0700

    implement grid drawing

--- designs.js
+++ designs.js
@@ -3,8 +3,27 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(e) {
+  const pixelCanvas = document.getElementById('pixelCanvas')
+  pixelCanvas.innerHTML = ''
+  const height = parseInt(sizePicker.height.value)
+  const width = parseInt(sizePicker.width.value)
 
-// Your code goes here!
+  for (let y = 0; y < height; y++) {
+    let row = document.createElement('tr')
+    row.setAttribute('id', `y-${y}`)
+    pixelCanvas.appendChild(row)
+    row = document.getElementById(`y-${y}`)
 
+    for (let x = 0; x < width; x++) {
+      let cell = document.createElement('td')
+      cell.setAttribute('id', `y-${y}x-${x}`)
+      row.appendChild(cell)
+    }
+  }
+  e.preventDefault()
 }
+
+const sizePicker = document.getElementById('sizePicker');
+
+sizePicker.addEventListener('submit', makeGrid);
